a4ece5dc5a54405745a602915885631fd21039c1
=elirehema <=rocksandystone@gmail.com>
2020-06-10 21:11:29 +0300

    Test deploy

--- app/test/index-spec.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const server = require('../index');
-
-describe('test', () => {
-  it('should return a string', () => {
-    expect('RestHub api started...').to.equal('RestHub api started...');
-  });
-});
---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'secret': 'supersecret',
+    // 1. MongoDB
+    REMOTE_MONGO_URI: process.env.REMOTE_MONGO_URI || "mongodb+srv://root:HRYIzBCIdvky4lwb@handmade-happiness-3skya.mongodb.net/test?retryWrites=true&w=majority",
+
+     // 1. Local MongoDB
+     LOCAL_MONGO_URI: process.env.LOCAL_MONGO_URI || 'mongodb://127.0.0.1/spring',
+
+     //Hosting PORT
+     HOSTING_PORT :  process.env.port || 8000,
+};
